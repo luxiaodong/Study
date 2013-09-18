ba657df0a39c8e068185e450cc098ebfff9123de
luxiaodong <luxiaodong@me.com>
2013-09-18 17:56:29 +0800

    add some function

--- computer/algorithm/src/GTree.cpp
+++ computer/algorithm/src/GTree.cpp
@@ -1,5 +1,100 @@
 #include "GTree.h"
+#include <QDebug>
 
-GTree::GTree()
+GTreeNode::GTreeNode()
 {
+    m_parent = 0;
+    m_left = 0;
+    m_right = 0;
 }
+
+void GBinarySearchTree::inorder_tree_walk(GTreeNode* root)
+{
+    if(root != 0)
+    {
+        this->inorder_tree_walk(root->m_left);
+        qDebug()<<root->m_value;
+        this->inorder_tree_walk(root->m_right);
+    }
+}
+
+GTreeNode* GBinarySearchTree::tree_seach(GTreeNode* root, int value)
+{
+    if(root != 0)
+    {
+        if(value == root->m_value)
+        {
+            return root;
+        }
+        else if(value > root->m_value)
+        {
+            return this->tree_seach(root->m_right, value);
+        }
+        else if(value < root->m_value)
+        {
+            return this->tree_seach(root->m_left, value);
+        }
+    }
+
+    return 0;
+}
+
+GTreeNode* GBinarySearchTree::iterative_tree_seach(GTreeNode* root, int value)
+{
+    GTreeNode* current = root;
+    while(current != 0)
+    {
+        if(value == current->m_value)
+        {
+            return current;
+        }
+        else if(value < current->m_value)
+        {
+            current = current->m_left;
+        }
+        else if(value > current->m_right)
+        {
+            current = current->m_right;
+        }
+    }
+
+    return 0;
+}
+
+GTreeNode* GBinarySearchTree::tree_minimum(GTreeNode* root)
+{
+    GTreeNode* current = root;
+    if(current != 0)
+    {
+        while(current->m_left != 0)
+        {
+            current = current->m_left;
+        }
+
+        return current;
+    }
+
+    return 0;
+}
+
+GTreeNode* GBinarySearchTree::tree_maximum(GTreeNode* root)
+{
+    GTreeNode* current = root;
+    if(current != 0)
+    {
+        while(current->m_right != 0)
+        {
+            current = current->m_right;
+        }
+
+        return current;
+    }
+
+    return 0;
+}
+
+GTreeNode* GBinarySearchTree::tree_successor(GTreeNode* root)
+{}
+
+GTreeNode* GBinarySearchTree::tree_predecessor(GTreeNode* root)
+{}
--- computer/algorithm/src/GTree.h
+++ computer/algorithm/src/GTree.h
@@ -1,10 +1,32 @@
 #ifndef GTREE_H
 #define GTREE_H
 
-class GTree
+class GTreeNode
 {
 public:
-    GTree();
+    GTreeNode();
+
+public:
+    GTreeNode* m_parent;
+    GTreeNode* m_left;
+    GTreeNode* m_right;
+    int m_value;
 };
+
+class GBinarySearchTree
+{
+public:
+    void inorder_tree_walk(GTreeNode* root);
+    GTreeNode* tree_seach(GTreeNode* root, int value);
+    GTreeNode* iterative_tree_seach(GTreeNode* root, int value);
+    GTreeNode* tree_minimum(GTreeNode* root);
+    GTreeNode* tree_maximum(GTreeNode* root);
+    GTreeNode* tree_successor(GTreeNode* root);
+    GTreeNode* tree_predecessor(GTreeNode* root);
+
+public:
+    GTreeNode*  m_root;
+};
+
 
 #endif // GTREE_H
